82dc08e5f0f8243fe1538f7486fa7522ff0756d2
MariaUgas <marria42@gmail.com>
2021-08-23 15:33:42 -0400

    eliminacion de imagen del slide

--- src/components/Galeria/Galeria.jsx
+++ src/components/Galeria/Galeria.jsx
@@ -4,24 +4,37 @@ import { Carousel } from "react-bootstrap";
 function Galeria() {
   return (
     <>
-      <section className='info-last'>
-        <div className='contenedor'>
-        <h2 class="titulo">Nuestro ambiente</h2>
-          <Carousel variant='dark'>
+      <section className="info-last">
+        <div className="contenedor">
+          <h2 class="titulo">Nuestro ambiente</h2>
+          <Carousel variant="dark">
             <Carousel.Item interval={800}>
-              <img className='d-block w-100 block-foto' src='img/foto1.webp' alt='slide 1' />
+              <img
+                className="d-block w-100 block-foto"
+                src="img/foto1.webp"
+                alt="slide 1"
+              />
             </Carousel.Item>
             <Carousel.Item interval={500}>
-              <img className='d-block w-100 block-foto' src='img/foto2.webp' alt='slide 2' />
+              <img
+                className="d-block w-100 block-foto"
+                src="img/foto2.webp"
+                alt="slide 2"
+              />
             </Carousel.Item>
             <Carousel.Item>
-              <img className='d-block w-100 block-foto' src='img/foto3.webp' alt='slide 3' />
+              <img
+                className="d-block w-100 block-foto"
+                src="img/foto3.webp"
+                alt="slide 3"
+              />
             </Carousel.Item>
             <Carousel.Item>
-              <img className='d-block w-100 block-foto' src='img/foto4.webp' alt='slide 4' />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img className='d-block w-100 block-foto' src='img/foto5.webp' alt='slide 6' />
+              <img
+                className="d-block w-100 block-foto"
+                src="img/foto4.webp"
+                alt="slide 4"
+              />
             </Carousel.Item>
           </Carousel>
         </div>
