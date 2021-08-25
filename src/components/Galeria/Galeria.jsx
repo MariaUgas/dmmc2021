9948ccc8f37e894082a25722fd95e19e93f78d21
dmmc2021 <dmmcdev2021@gmail.com>
2021-08-24 21:16:24 -0400

    actualizacion galeria

--- src/components/Galeria/Galeria.jsx
+++ src/components/Galeria/Galeria.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
+import foto1 from "../../img/foto1.webp";
+import foto2 from "../../img/foto2.webp";
+import foto3 from "../../img/foto3.webp";
+import foto4 from "../../img/foto4.webp";
+
 
 function Galeria() {
   return (
@@ -9,21 +14,18 @@ function Galeria() {
         <h2 class="titulo">Nuestro ambiente</h2>
           <Carousel variant='dark'>
             <Carousel.Item interval={800}>
-              <img className='d-block w-100 block-foto' src='img/foto1.webp' alt='slide 1' />
+              <img className='d-block w-100 block-foto' src={foto1} alt='slide 1' />
             </Carousel.Item>
             <Carousel.Item interval={500}>
-              <img className='d-block w-100 block-foto' src='img/foto2.webp' alt='slide 2' />
+              <img className='d-block w-100 block-foto' src={foto2} alt='slide 2' />
             </Carousel.Item>
             <Carousel.Item>
-              <img className='d-block w-100 block-foto' src='img/foto3.webp' alt='slide 3' />
+              <img className='d-block w-100 block-foto' src={foto3} alt='slide 3' />
             </Carousel.Item>
             <Carousel.Item>
-              <img className='d-block w-100 block-foto' src='img/foto4.webp' alt='slide 4' />
+              <img className='d-block w-100 block-foto' src={foto4} alt='slide 4' />
             </Carousel.Item>
-            <Carousel.Item>
-              <img className='d-block w-100 block-foto' src='img/foto5.webp' alt='slide 6' />
-            </Carousel.Item>
-          </Carousel>
+           </Carousel>
         </div>
       </section>
     </>
