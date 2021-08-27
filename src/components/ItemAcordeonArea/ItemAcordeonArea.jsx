c577632b8e72dfde11415074d1f320406955c225
MariaUgas <marria42@gmail.com>
2021-08-26 23:55:53 -0400

    Componente dinamico de acordion incompleto

--- src/components/ItemAcordeonArea/ItemAcordeonArea.jsx
+++ src/components/ItemAcordeonArea/ItemAcordeonArea.jsx
@@ -1,55 +1,51 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import store from "../../firebase/firebase.js";
-import { Accordion } from 'react-bootstrap';
+import { Accordion } from "react-bootstrap";
 import TablaCursos from "../TablaCursos/TablaCursos.jsx";
 
+export const ItemAcordeonArea = () => {
+  const [areasObj, setAreasObj] = useState([]);
 
-export const ItemAcordeonArea=()=>{
-    const[areas, setAreas] = useState([]);
+  useEffect(() => {
+    store.collection("areas").onSnapshot((snap) => {
+      const documents = [];
+      snap.forEach((doc) => {
+        documents.push({ id: doc.id, ...doc.data() });
+      });
+      setAreasObj(documents);
+    });
+  }, []);
 
-    useEffect(()=>{
-        store.collection("areas")
-        .onSnapshot(snap=>{
-            const documents=[];
-            snap.forEach(doc=>{
-            documents.push({id:doc.id, ...doc.data() })
-        });
-        setAreas(documents)
-    })
-}, [])
+  console.log("ItemAcordeonArea=>", areasObj);
 
-console.log(areas)
-
-
-
-    return(
-
-        <div className="contenedor-cursos" id="cursos-id">
-
-    <h3 className="titulo">TE OFRECEMOS:</h3>
-        <Accordion flush >
-            {
-                areas.map((area, indice)=>{
-                    console.log(area);
-                    <Accordion.Item eventKey={indice}>
-                    <Accordion.Header>{area.nombre}</Accordion.Header>
-                    <Accordion.Body style={{textAlign:"left"}}>
-                      
-                      <TablaCursos idarea={area.codigo} />
-                    
-                    </Accordion.Body>
-                  </Accordion.Item>
-                })
-            }
-        </Accordion>
+  const arrayAreas = areasObj.map((area) => {
+    const { areas } = area;
+    return areas;
+  });
+  console.log("AcordeonArea=>", arrayAreas);
+  return (
+    <div className="contenedor-cursos" id="cursos-id">
+      <h3 className="titulo">TE OFRECEMOS:</h3>
+      <Accordion flush>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Contabilidad</Accordion.Header>
+          <Accordion.Body style={{ textAlign: "left" }}>
+            <TablaCursos idarea="CON00" />
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      {/* <Accordion flush>
+        {arrayAreas.map((elemArea, index) => {
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>{elemArea[index].nombre}</Accordion.Header>
+            <Accordion.Body style={{ textAlign: "left" }}>
+              <TablaCursos idarea={elemArea[index].codigo} />
+            </Accordion.Body>
+          </Accordion.Item>;
+        })}
+      </Accordion> */}
     </div>
-        
-     
-    )
-
-    
-
-
-}
+  );
+};
 
 export default ItemAcordeonArea;
